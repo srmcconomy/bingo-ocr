b282d0bc0547f93a56a24f3688266c680010b15f
Steven McConomy <srmcconomy@gmail.com>
2016-02-12 02:06:37 -0500

    Asda

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser')
 app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -166,7 +168,7 @@ app.post('/', function(req, res) {
   });
 })
 
-app.listen(3000)
+app.listen(port)
 
 
 //
